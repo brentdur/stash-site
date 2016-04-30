3403f631c8186669d707c4f85d6e07251d3da53d
Brenton Durkee <brentdur@gmail.com>
2016-04-30 19:47:19 +0100

    Changes

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// $(document).ready(function() {
+// 	$('#hero').height($(window).height() - 85 - 65 - 45);
+// 	$(window).resize(function() {
+// 	    $('#hero').height($(window).height() - 85 - 65 - 45);
+// 	}).resize();
+// })
+
